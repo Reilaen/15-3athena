8eeb685bbd8a511abf1d606762cf556713ad722c
Oracle <octavia.kitsune@gmail.com>
2026-05-05 03:40:43 +0200

    Fix for a server crash if you reloaded scripts and then entered an instance.

--- src/map/instance.c
+++ src/map/instance.c
@@ -26,6 +26,7 @@
 #include <stdarg.h>
 #include <time.h>
 
+struct instance_data *instances = NULL;
 unsigned short instance_start_id = 0;
 unsigned short instance_count = 0;
 
@@ -760,8 +761,10 @@ void do_final_instance(void)
 	for( i = 0; i < instance_count; i++ )
 		instance_destroy(i);
 
-	if (instances)
+	if (instances) {
 		aFree(instances);
+		instances = NULL;
+	}
 
 	instance_count = 0;
 }
